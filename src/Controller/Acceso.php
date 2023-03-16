2095ce7689cfe6a35cdb1d0354dcf417f65bddae
Willy Mattos <willmattos.c@gmail.com>
2023-03-16 17:22:38 +0100

    creacion

---
+++ src/Controller/Acceso.php
@@ -0,0 +1,12 @@
+<?php 
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class Acceso extends AbstractController{
+  
+}
